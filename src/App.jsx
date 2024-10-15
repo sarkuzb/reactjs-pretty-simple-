b9d47d28456604daeb73a68de1678a6e355f2f46
sarkuzb <you@example.comssmartprinter@gmail.com>
2024-10-15 10:12:16 +0300

    main

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Second from './SecondApp.jsx'
+
+function App() {
+  return(
+    <Second/>
+  );
+}
+
+export default App
---
+++ src/SecondApp.jsx
@@ -0,0 +1,13 @@
+import profilepic from './assets/df3ic0p-67327a2f-f1a2-4d65-93fb-6aaf6116b290.jpg'
+
+function Second(){
+  return(
+    <div className="second">
+      <img className="image" src={profilepic} alt="" />
+      <h1>sar1k</h1>
+      <p>500 hours gameplay</p>
+    </div>
+  );
+}
+
+export default Second
